2c48ac21df3c8cc9ca43555fad6de0c01ebf5c80
gerwin90 <60857296+gerwin90@users.noreply.github.com>
2025-05-28 14:00:34 +0100

    Update main.tf

--- s3/main.tf
+++ s3/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "spacelift_bucket" {
-  bucket = "spacelift-gkirwan-bucket"
+  bucket = "newspacelift-gkirwan-bucket"
   
   tags = {
     Name        = "spacelift_gkirwan_bucket"
