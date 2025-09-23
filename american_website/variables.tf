b8e2c44372f92b4e32548fdabd2b332b5a3b8651
Gerard Kirwan <gkirwan@unum.com>
2025-09-23 11:09:32 -0400

    Adding in American Website

--- american_website/variables.tf
+++ american_website/variables.tf
@@ -1,11 +1,10 @@
 variable "aws_region" {
   type        = string
-  default     = "us-east-1"
   description = "AWS region for all resources"
 }
 
 variable "bucket_name" {
   type        = string
   description = "Globally unique S3 bucket name"
-  # No default - force users to provide their own unique name
+  default = "our-american-website-bucket"
 }
