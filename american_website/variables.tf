cf79307b306b5c5a143408461de9250a487df4fd
gerwin90 <60857296+gerwin90@users.noreply.github.com>
2025-09-30 12:32:12 -0400

    Update variables.tf

--- american_website/variables.tf
+++ american_website/variables.tf
@@ -9,3 +9,15 @@ variable "bucket_name" {
   description = "Globally unique S3 bucket name"
   default = "twohumansonetailinamerica-bucket"
 }
+
+variable "domain_name" {
+  description = "Domain name for the website"
+  type        = string
+  default     = "twohumansonetailinamerica.com"
+}
+
+variable "certificate_arn" {
+  description = "ACM certificate ARN (must be in us-east-1)"
+  type        = string
+  default     = "arn:aws:acm:us-east-1:310649825077:certificate/c3b9ce2e-6f9f-4b09-a339-3f2249dbb668"
+}
